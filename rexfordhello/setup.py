0f6faa83b73387447788d5d7a9916a82cb54244a
Rexford Cristal <rcristal3@netscape.net>
2019-03-04 03:05:45 -0500

    first commit

---
+++ rexfordhello/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='rexfordhello',
+       version='0.1',
+       description='This module greets the user.',
+       url='',
+       author='Rexford Cristal',
+       author_email='rexford.cristal@gmail.com',
+       license='MIT',
+       packages=['rexfordhello'],
+       install_requires=[ ],
+       zip_safe=False )
